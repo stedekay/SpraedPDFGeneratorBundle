958ae7b18a6002d76c2c5d0bfc8e394ee99b2505
Stefan Blanke <stefblanke@gmail.com>
2012-04-12 21:43:42 +0200

    added bootstrap.php and vendors.php to enable tests in travis ci

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,17 +1,7 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Spraed\\PDFGeneratorBundle\\') &&
-        file_exists($file = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php')) {
-        require_once $file;
-    }
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
