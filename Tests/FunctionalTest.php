227b8f14adaf8d92356541840d1b881b14d086e0
stedekay <blanke@enterio.de>
2013-09-23 16:44:07 +0200

    extended functional test from WebTestCase

--- Tests/FunctionalTest.php
+++ Tests/FunctionalTest.php
@@ -3,9 +3,11 @@
 namespace Spraed\PDFGeneratorBundle\Tests;
 
 use Spraed\PDFGeneratorBundle\PDFGenerator\PDFGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\AppKernel;
 use Symfony\Component\HttpFoundation\Response;
 
-class FunctionalTest extends \PHPUnit_Framework_TestCase
+class FunctionalTest extends WebTestCase
 {
     public function setUp()
     {
@@ -17,7 +19,7 @@ class FunctionalTest extends \PHPUnit_Framework_TestCase
 
     public function testCreateTemporaryFile()
     {
-        $pdfGenerator = new PDFGenerator();
+        $pdfGenerator = new PDFGenerator(self::$kernel);
         $pdf = $pdfGenerator->generatePDF(__DIR__ . '/Resources/test-without-resource-links.html');
 
         $response = new Response($pdf,
