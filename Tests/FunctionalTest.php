8f1aa68b017cfdb3a0d93bf2a6d18f61550f17d0
stedekay <blanke@enterio.de>
2013-09-23 17:56:40 +0200

    removed functional test, assertion was always true, needs to be fixed

--- Tests/FunctionalTest.php
+++ Tests/FunctionalTest.php
@@ -18,19 +18,19 @@ class FunctionalTest extends WebTestCase
 
     public function testCreateTemporaryFile()
     {
-        $client = static::createClient();
-        $kernel = $client->getKernel();
-        $pdfGenerator = new PDFGenerator($kernel);
-        $pdf = $pdfGenerator->generatePDF(__DIR__ . '/Resources/test-without-resource-links.html');
-
-        $response = new Response($pdf,
-            200,
-            array(
-                'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'inline; filename="out.pdf"'
-            ));
-
-        $this->assertEquals(200, $response->getStatusCode());
+//        $client = static::createClient();
+//        $kernel = $client->getKernel();
+//        $pdfGenerator = new PDFGenerator($kernel);
+//        $pdf = $pdfGenerator->generatePDF(__DIR__ . '/Resources/test-without-resource-links.html');
+//
+//        $response = new Response($pdf,
+//            200,
+//            array(
+//                'Content-Type' => 'application/pdf',
+//                'Content-Disposition' => 'inline; filename="out.pdf"'
+//            ));
+//
+//        $this->assertEquals(200, $response->getStatusCode());
     }
 
 }
